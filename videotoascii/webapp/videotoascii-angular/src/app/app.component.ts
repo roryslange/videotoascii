bf05496860d7dfd101c3b02f6ae1ad3c74a006cf
rory lange <rorystrattonlange@gmail.com>
2025-07-15 22:26:57 -0400

    add just the first two components

--- videotoascii/webapp/videotoascii-angular/src/app/app.component.ts
+++ videotoascii/webapp/videotoascii-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from '../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- videotoascii/webapp/videotoascii-angular/src/components/navbar/navbar.component.spec.ts
+++ videotoascii/webapp/videotoascii-angular/src/components/navbar/navbar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarComponent } from './navbar.component';
+import { Navbar } from './navbar.component';
 
 describe('NavbarComponent', () => {
-  let component: NavbarComponent;
-  let fixture: ComponentFixture<NavbarComponent>;
+  let component: Navbar;
+  let fixture: ComponentFixture<Navbar>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavbarComponent]
+      imports: [Navbar]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(NavbarComponent);
+    fixture = TestBed.createComponent(Navbar);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
